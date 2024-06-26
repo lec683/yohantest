24c7e0e73682e30d82c44cadfdf7077f49b0dbd9
lec683 <lector683@gmail.com>
2024-06-26 16:06:27 +0300

    Update index.js

--- index.js
+++ index.js
@@ -23,22 +23,27 @@ client.on('qr', qr => {
 
 client.initialize();
 // Listening to all incoming messages
+
 client.on('message_create', (async message => {
+	let msgtime = ((((new Date(message.timestamp * 1000)).toLocaleString({ timeStyle: "short" })).split(' ')[1]).split(':')).slice(0, 2)
+    	msgtime = `${msgtime[0]}:${msgtime[1]}`
+	console.log(`[${msgtime}][${message.deviceType}]${message.from}:>>[${message.author}]: ${message.body}`)
+	const chat = await message.getChat();
 	if (message.body === '!ping') {
 		// send "pong" directly to the message
 		client.sendMessage(message.from, 'pong');
          }
 
 	//اكتب الامر اللي يشغل المنشن المخفي مثلا -- !مخفي او #منشن او او او و	
-	else if (msg.body.startsWith('(الامر)' && message.fromMe)) { //hidden mentions with a coustm message
+	else if (message.body.startsWith('.مخفي ') && message.fromMe) { //hidden mentions with a coustm message
 
         const chat = await message.getChat();
 
-        let text = message.body.slice(5); // هنا تكتب عدد الحروف الموجوده في الامر مع احتساب المسافة عشان يقص الرساله و يحط عليها منشن مخفي
+        let text = message.body.slice(6); // هنا تكتب عدد الحروف الموجوده في الامر مع احتساب المسافة عشان يقص الرساله و يحط عليها منشن مخفي
 
         let mentions = [];
 
-        let groupid = message.from;
+        let groupid = chat.id._serialized;
 
 
 
